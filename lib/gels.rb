bc4e372541f77be11ec7149b0b6625a852acac88
Harry Grillo <harry@redbassett.com>
2015-03-18 03:17:37 -0400

    Re-add removed compiled gels
    Adding an issue for the compiler deleting everything if no files
    are specified at the command line. Probably default behaviour will
    be to compile everything in the data directory?

--- lib/gels.rb
+++ lib/gels.rb
@@ -1,4 +1,14 @@
 def gels
   [
+    { code: 'R00', name: 'Dempster Open', rgb: { red: 255, green: 255, blue: 255 } },
+    { code: 'R01', name: 'Light Bastard Amber', rgb: { red: 255, green: 169, blue: 99 } },
+    { code: 'R02', name: 'Bastard Amber', rgb: { red: 255, green: 161, blue: 84 } },
+    { code: 'R10', name: 'Medium Yellow', rgb: { red: 255, green: 248, blue: 31 } },
+    { code: 'R312', name: 'Canary', rgb: { red: 255, green: 195, blue: 31 } },
+    { code: 'R324', name: 'Gypsy Red', rgb: { red: 255, green: 0, blue: 0 } },
+    { code: 'R39', name: 'Skelton Exotic Sangria', rgb: { red: 255, green: 64, blue: 255 } },
+    { code: 'R64', name: 'Light Steel Blue', rgb: { red: 56, green: 199, blue: 255 } },
+    { code: 'R371', name: 'Theatre Booster 1', rgb: { red: 214, green: 186, blue: 255 } },
+    { code: 'R74', name: 'Night Blue', rgb: { red: 30, green: 5, blue: 255 } }
   ]
 end
